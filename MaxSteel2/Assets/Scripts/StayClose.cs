4aa11ab0f446bef5645a4713778d9471a4c3792d
Kaduco19 <54106253+Kaduco19@users.noreply.github.com>
2019-10-21 16:07:55 -0300

    16:07
    21/10/2019

---
+++ MaxSteel2/Assets/Scripts/StayClose.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StayClose : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string personagem;
+    public Vector2 LocalPersonagem;
+    private GameObject character;
+    private bool bateu = false;
+    private bool E = false;
+    // public Collider2D other;
+
+    // private SceneManagement sceneManagement = new sceneManagement();
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+            Debug.Log("bateu no trigger");
+        GameObject canvas2 = character.transform.Find("Canvas2").gameObject;
+        if (other.CompareTag(personagem) && !other.isTrigger)
+            {
+            bateu = true;
+            canvas2.SetActive(true);
+        }
+        
+    }
+   private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("ksksksks");
+        GameObject canvass = character.transform.Find("Canvass").gameObject;
+        GameObject canvas2 = character.transform.Find("Canvas2").gameObject;
+        bateu = false;
+        canvass.SetActive(false);
+        canvas2.SetActive(false);
+    }
+
+    public void Update()
+    {
+        character = GameObject.FindGameObjectWithTag("CanvasCharacter");
+        GameObject canvass = character.transform.Find("Canvass").gameObject;
+        GameObject canvas2 = character.transform.Find("Canvas2").gameObject;
+
+        if (bateu)
+        {
+            //canvas2.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                canvas2.SetActive(false);
+                canvass.SetActive(true);
+            }
+        }
+        
+    }
+}
---
+++ MaxSteel2/Assets/Scripts/charge_text.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class charge_text : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float delay = 0.1f;
+    public string FullText;
+    private string currentText = "";
+    void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    // Update is called once per frame
+    IEnumerator ShowText()
+    {
+        for(int i = 0; i < FullText.Length; i++)
+            {
+            currentText = FullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+
+            }
+
+    }
+}
